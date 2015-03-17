310b47283a86bf265c53827f08d0a65633f78a91
gneuvill <gregoire.neuville@gmail.com>
2015-03-17 15:16:55 +0100

    misc

--- integration/integration_livraison.sh
+++ integration/integration_livraison.sh
@@ -6,28 +6,28 @@
 #              - UPDATE Schema Doctrine
 #
 ##########################################################################
-REPERTOIRE_WWW="/root/Workspace/apache-data/matrix-backend"
-REPERTOIRE_JENKINS="/var/lib/jenkins/workspace/INT_Matrix_Backend"
+REPERTOIRE_WWW="/data/htdocs"
+REPERTOIRE_JENKINS="/data/webapps/jenkins.univ-rennes1.fr/conf/jobs/formation"
 
 ## INSTALLATION SUR LE SERVEUR D'INTEGRATION
-CMD="rm -rf ${REPERTOIRE_WWW} ; "
-CMD="$CMD mkdir ${REPERTOIRE_WWW} ; "
-CMD="$CMD cp -r ${REPERTOIRE_JENKINS}/* ${REPERTOIRE_WWW} ; "
-CMD="$CMD chown -R www-data:www-data ${REPERTOIRE_WWW} ; "
-CMD="$CMD chmod -R u+rw ${REPERTOIRE_WWW} ; "
+#CMD="rm -rf ${REPERTOIRE_WWW} ; "
+#CMD="$CMD mkdir ${REPERTOIRE_WWW} ; "
+CMD="$CMD scp -r ${REPERTOIRE_JENKINS} apache@vmjava-pdevq1/${REPERTOIRE_WWW} ; "
+#CMD="$CMD chown -R www-data:www-data ${REPERTOIRE_WWW} ; "
+#CMD="$CMD chmod -R u+rw ${REPERTOIRE_WWW} ; "
 
-echo $CMD | ssh root@127.0.0.1
+#echo $CMD | ssh root@127.0.0.1
 
 
 ## UPDATE Schema Doctrine
-CMD="su www-data ; "
-CMD="$CMD cd  ${REPERTOIRE_JENKINS}/website ; "
-CMD="$CMD echo 'drop database matrix' | mysql --user=root --password=matrix --host=db ; "
-CMD="$CMD php app/console doctrine:database:create ; "
-CMD="$CMD php app/console doctrine:schema:update --force ; "
-CMD="$CMD php app/console doctrine:fixtures:load --fixtures=src/ICert/Matrix/RestAPIBundle/Tests/DataFixtures ; "
-
-echo $CMD | ssh root@127.0.0.1
+#CMD="su www-data ; "
+#CMD="$CMD cd  ${REPERTOIRE_JENKINS}/website ; "
+#CMD="$CMD echo 'drop database matrix' | mysql --user=root --password=matrix --host=db ; "
+#CMD="$CMD php app/console doctrine:database:create ; "
+#CMD="$CMD php app/console doctrine:schema:update --force ; "
+#CMD="$CMD php app/console doctrine:fixtures:load --fixtures=src/ICert/Matrix/RestAPIBundle/Tests/DataFixtures ; "
+#
+#echo $CMD | ssh root@127.0.0.1
 
 
 ## Modification des parameters pour la production (Information dans le virtualhost apache)"
@@ -38,9 +38,9 @@ echo $CMD | ssh root@127.0.0.1
 
 
 ## Redémarrage des containers
-CMD="docker restart db web"
-
-echo $CMD | ssh root@127.0.0.1
+#CMD="docker restart db web"
+#
+#echo $CMD | ssh root@127.0.0.1
 
 
 
